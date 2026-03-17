06984ab8a0e7eab4d26284e49a139663eab0f40b
Hariharan3624 <vikrammanoharan@gmail.com>
2026-03-17 19:08:39 +0000

    Add files via upload

---
+++ list2.py
@@ -0,0 +1,11 @@
+def match(words):
+    ctr = 0
+    lst =[]
+    for word in words:
+        if len(word)>1 and word[0] == word[-1]:
+            ctr += 1
+            lst.append(word)
+    print("list of words with first and last character same",lst)
+    return ctr
+count = match(["abc","cfc","xyz","aba","1221"])
+print("number of words having first and last character same:", count)

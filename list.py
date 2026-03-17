3530fa3f5fb75069e64bb88fb12c7a9d363026f0
Hariharan3624 <vikrammanoharan@gmail.com>
2026-03-17 18:51:52 +0000

    Add files via upload

---
+++ list.py
@@ -0,0 +1,9 @@
+empty_list= []
+print()
+numbers = [1,2,3,4,5,6]
+print(numbers)
+triples = ["Hariharan",6,"Emma"]*7
+print(triples)
+aList = [1000,2000,3000,4000,5000,6000]
+aList = aList[::-1]
+print(aList,"\n") 

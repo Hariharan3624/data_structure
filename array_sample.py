38fa4e12af39720cf821260b836b0f0f50a98098
Hariharan3624 <vikrammanoharan@gmail.com>
2026-03-26 19:02:30 +0000

    Add files via upload

---
+++ array_sample.py
@@ -0,0 +1,16 @@
+import array as arr
+a = arr.array("i",[1,2,3])
+print("the new created array is: ",end = "")
+for i in range (0,3):
+    print(a[i],end ="")
+print()
+b = arr.array("d",[2.5,3.2,3.3])
+print("the new created array is:",end ="")
+for i in range (0,3):
+    print(b[i], end ="")
+a.insert(1,4)
+print("Array after insertion: ",end="")
+for i in (a):
+    print(i,end = "")
+print()
+b.append(4.4)

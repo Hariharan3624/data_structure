5feb488a74c88d4029d2343c7d0c611200811810
Hariharan3624 <vikrammanoharan@gmail.com>
2026-03-26 18:36:14 +0000

    Add files via upload

---
+++ set_activity.py
@@ -0,0 +1,14 @@
+set1 = {1,2,3}
+print(set1)
+set1 = {1.0,"Hello",(1,2,3)}
+print(set1)
+set1 = {1,2,3,4,3,2}
+print(set1)
+set1 = set([1,2,3,4])
+print(set1)
+num = set([1,2,3,4,5])
+print("original set: ")
+print(num)
+num.pop()
+print("after removing the first element from the said set: ")
+print(num)
